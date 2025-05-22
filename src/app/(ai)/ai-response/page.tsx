0d807f7c555c05114350c5ad7a0889740e8c7c70
Jayendra <jayendra.citrusbug@gmail.com>
2025-05-22 11:05:05 +0530

    Fix error

--- src/app/(ai)/ai-response/page.tsx
+++ src/app/(ai)/ai-response/page.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import { AIResponse } from "@/components/Organism/ai-response";
 
 const page = () => {
-  return <div className="p-4"><AIResponse /></div>;
+  return (
+    <div className="p-4">
+      <Suspense fallback={<div>Loading...</div>}>
+        <AIResponse />
+      </Suspense>
+    </div>
+  );
 };
 
 export default page;
